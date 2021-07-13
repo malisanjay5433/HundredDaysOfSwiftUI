d6159e0195cc71296dbc23ef5d4819fb0a2241f8
= <malisanjay5433@gmail.com>
2021-07-13 17:01:04 +0530

    inital commit

---
+++ HundredDaysOfSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by Sanjay Mali on 13/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HundredDaysOfSwiftUI/HundredDaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDaysOfSwiftUIApp.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by Sanjay Mali on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct HundredDaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
